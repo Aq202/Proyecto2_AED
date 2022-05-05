e99298ad93d2dd56fc903ca7743bc32fd2af34c5
pabloozamora <pabloozamora02@gmail.com>
2022-05-05 16:21:43 -0600

    Barra de navegación
    Implementado el funcionamiento y estilo de la Navbar

---
+++ frontend-design/src/components/NavBar.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useRef, useState } from "react";
+import "../styles/navBar.css";
+import { NavLink } from "react-router-dom";
+import { CgMenuRound } from "react-icons/cg";
+import { IoMdCloseCircleOutline } from "react-icons/io";
+
+const NavBar = () => {
+	const navBarRef = useRef();
+
+	const [showCloseButton, setShowCloseButton] = useState(false);
+
+	useEffect(() => {
+		window.onscroll = e => {
+			if (window.scrollY > 50) navBarRef.current.classList.add("backgroundStyle");
+			else navBarRef.current.classList.remove("backgroundStyle");
+		};
+	}, []);
+
+	const handleShowMenu = () => {
+		navBarRef.current.classList.toggle("showMenu");
+
+		if (navBarRef.current.classList.contains("showMenu")) setShowCloseButton(true);
+		else setShowCloseButton(false);
+	};
+
+	const handlePageChange = () => {
+		if (navBarRef.current.classList.contains("showMenu")){
+			navBarRef.current.classList.remove("showMenu");
+            setShowCloseButton(false);
+        }
+	};
+
+	return (
+		<nav id="navBar" ref={navBarRef}>
+			<ul className="menu">
+				<li>
+					<NavLink to="/profile" onClick={handlePageChange}>
+						Perfil
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to="/newMovie" onClick={handlePageChange}>
+						Nueva Película
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to="/recomendations" onClick={handlePageChange}>
+						Recomendaciones
+					</NavLink>
+				</li>
+			</ul>
+			<ul className="simpleMenu">
+				<li onClick={handleShowMenu}>
+					{!showCloseButton ? (
+						<CgMenuRound className="icon" />
+					) : (
+						<IoMdCloseCircleOutline className="icon" />
+					)}
+				</li>
+			</ul>
+		</nav>
+	);
+};
+
+export default NavBar;
