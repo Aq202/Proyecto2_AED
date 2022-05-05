3cfc6755415e68902cf0bb6cb3457ff7dfa64ad2
pabloozamora <pabloozamora02@gmail.com>
2022-05-05 17:25:09 -0600

    Página de perfil
    Funcionamiento y estilos de la página de perfil finalizados

---
+++ frontend-design/src/components/ProfilePage.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "../styles/profilePage.css";
+import Banner from "./Banner.js";
+import profileBanner from "../resources/images/profileBanner.svg";
+import countries from "../resources/files/countries.json";
+import ProfileForm from "./ProfileForm";
+
+const ProfilePage = () => {
+	console.log(countries);
+	return (
+		<div id="profilePage">
+			<Banner bannerImage={profileBanner} title="Perfil de Usuario" />
+			<div className="body-page">
+				<p className="instruccions">
+					Por favor, ingresa tu información personal para que podamos realizar mejores
+					recomendaciones que se adapten de mejor manera a tí.
+				</p>
+
+				<ProfileForm />
+			</div>
+		</div>
+	);
+};
+
+export default ProfilePage;
